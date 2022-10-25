8b69ed3ef501be54078f285a436f0b397695e298
omajoor <olafmajoor@hotmail.com>
2022-10-25 10:44:46 +0200

    split classes in own file

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 
 const gravity = 0.7
 
+const background = new Sprite({
+    position: {
+        x: 0,
+        y: 0,
+    },
+    imageSrc: './img/background.png'
+})
 
-
-const player = new Sprite({
+const player = new Fighter({
     position: {
         x: 0,
         y:0,
@@ -26,7 +32,7 @@ const player = new Sprite({
     color: 'red'
 })
 
-const enemy = new Sprite({
+const enemy = new Fighter({
     position: {
         x: 400,
         y: 100,
@@ -100,6 +106,7 @@ function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0,0,canvas.width,canvas.height)
+    background.update() 
     player.update()
     enemy.update()
 
